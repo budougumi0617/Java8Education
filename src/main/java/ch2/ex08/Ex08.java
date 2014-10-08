faac4fbe6076158f87e9e7d393c659633e3633f2
budougumi0617 <budougumi0617@gmail.com>
2014-10-09 08:41:21 +0900

    Add ex02_08

---
+++ src/main/java/ch2/ex08/Ex08.java
@@ -0,0 +1,35 @@
+/**
+ * @file
+ * @par File Name:
+ * Ex08.java
+ * @author budougumi0617
+ * @date Created on 2014/10/09
+ */
+package main.java.ch2.ex08;
+
+import java.util.stream.Stream;
+
+/**
+ * @author budougumi0617
+ * @note public static <T> Stream<T> zip(Stream<T> first, Stream<T>
+ *       second)メソッドを作成しなさい。 そのメソッドは、 ストリームであるfirstとsecondから要素を交互に取り出し、
+ *       どちらかのストリームから要素がなくなったら停止します。
+ */
+public class Ex08 {
+    /**
+    *
+    * @param first
+    * @param second
+    * @return firstとsecondから要素を交互に取り出し、 どちらかのストリームから要素がなくなったら停止する
+    */
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+        // TODO Empty method
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // TODO Empty method
+
+    }
+
+}
