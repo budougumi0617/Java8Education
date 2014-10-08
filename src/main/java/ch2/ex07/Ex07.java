72e92ebfe865236a136f1904e16f1c7714b04485
budougumi0617 <budougumi0617@gmail.com>
2014-10-09 08:35:36 +0900

    Add ex02_07 and test cases

---
+++ src/main/java/ch2/ex07/Ex07.java
@@ -0,0 +1,29 @@
+/**
+ * @file
+ * @par File Name:
+ * Ex07.java
+ * @author budougumi0617
+ * @date Created on 2014/10/09
+ */
+package main.java.ch2.ex07;
+
+import java.util.stream.Stream;
+
+/**
+ * @author budougumi0617
+ * @note 上司が、メソッドとして public static <T> boolean isFinite( Stream<T>stream)を
+ *       作成するように求めています。 それは、よくない考えでしょうか。
+ *       まずは作成してみてから、考えてみなさい。
+ */
+public class Ex07 {
+/**
+ * Finite...限界のある、限定された、限りある、有限の
+ */
+    public static <T> boolean isFinite(Stream<T> stream) {
+        //TODO Empty method.
+        return false;
+}
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/test/java/ch2/ex07/Ex07Test.java
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * @par File Name:
+ * Ex07Test.java
+ * @author budougumi0617
+ * @date Created on 2014/10/09
+ */
+package test.java.ch2.ex07;
+
+import static org.junit.Assert.*;
+
+import java.util.stream.Stream;
+
+import main.java.ch2.ex07.Ex07;
+
+import org.junit.Test;
+
+/**
+ * @author budougumi0617
+ * @note 無限ストリームと有限ストリームをisFinite()に与えてみる。
+ */
+public class Ex07Test {
+
+    @Test
+    public void testGetTrue() {
+        Stream<String> infiniteStream = Stream.generate(() -> "test");
+        assertTrue(Ex07.isFinite(infiniteStream));
+    }
+
+    @Test
+    public void testGetFalse() {
+        Stream<String> finiteStream = Stream.generate(() -> "test").limit(10);
+        assertFalse(Ex07.isFinite(finiteStream));
+    }
+
+}
