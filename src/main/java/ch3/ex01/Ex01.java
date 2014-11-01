093e8bccf9fcca756b9529b361bf0154860f6a96
budougumi0617 <budougumi0617@gmail.com>
2014-11-01 13:43:57 +0900

    Delete unneed comments

--- src/main/java/ch3/ex01/Ex01.java
+++ src/main/java/ch3/ex01/Ex01.java
@@ -14,39 +14,36 @@ import java.util.logging.Logger;
 
 /**
  * @author budougumi0617
- * @note 条件的なロギングを提供することで、遅延ロギング技法を強化しなさい。 典型的な呼び出しは、 logIf(Level.FINEST, () ->
- *       i == 10, () -> "a[10] = " + a[1]) となります。
- *       ロガーがメッセージをロギングしないのであれば、その条件を評価しないようにしなさい。 Loggerのレベルは降順で次のとおりです。 SEVERE
- *       (最高値), WARNING (警告), INFO, CONFIG, FINE, FINER, FINEST (最低値)
+ * @note 条件的なロギングを提供することで、遅延ロギング技法を強化しなさい。
+ *       典型的な呼び出しは、 logIf(Level.FINEST, () -> i ==
+ *       10, () -> "a[10] = " + a[1]) となります。
+ *       ロガーがメッセージをロギングしないのであれば、その条件を評価しないようにしなさい。
+ *       Loggerのレベルは降順で次のとおりです。 SEVERE (最高値),
+ *       WARNING (警告), INFO, CONFIG, FINE, FINER,
+ *       FINEST (最低値)
  * @see <a
  *      href="http://docs.oracle.com/javase/jp/8/api/java/util/function/package-frame.html">Supplier系</a>
  * @see test.java.ch3.ex01.Ex01Test
  */
 public class Ex01 {
-    static private Logger logger = Logger.getLogger(Ex01.class.getName());
+	static private Logger logger = Logger.getLogger(Ex01.class.getName());
 
-    public static void setLoggerLevel(Level level) {
-        Ex01.logger.setLevel(level);
-    }
+	public static void setLoggerLevel(Level level) {
+		Ex01.logger.setLevel(level);
+	}
 
-    public static void logIf(Level level, BooleanSupplier condition,
-            Supplier<String> message) {
-        System.out.println("Called Method");
-        if (logger.isLoggable(level)) {
-            System.out.println("Cheked Logging Level");
-            if (condition.getAsBoolean()) {
-                System.out.println("Cheked condition");
-                logger.info(message.get());
-            }
-        }
-    }
+	public static void logIf(Level level, BooleanSupplier condition,
+			Supplier<String> message) {
+		if (logger.isLoggable(level)) {
+			if (condition.getAsBoolean()) {
+				logger.info(message.get());
+			}
+		}
+	}
 
-    /**
-    * @param args
-    */
-    public static void main(String[] args) {
-        Ex01.setLoggerLevel(Level.FINE);
-        logIf(Level.SEVERE, () -> true, () -> "test");
-    }
+	public static void main(String[] args) {
+		Ex01.setLoggerLevel(Level.FINE);
+		logIf(Level.SEVERE, () -> true, () -> "test");
+	}
 
 }
