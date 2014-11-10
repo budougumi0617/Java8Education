14585fe16a349c5a56010397ff804d670d87e479
budougumi0617 <budougumi0617@gmail.com>
2014-11-10 19:27:06 +0900

    Modify test

--- src/test/java/ch3/ex07/Ex07Test.java
+++ src/test/java/ch3/ex07/Ex07Test.java
@@ -7,8 +7,8 @@
  */
 package test.java.ch3.ex07;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.Arrays;
 
@@ -22,93 +22,85 @@ import org.junit.Test;
  * @note Ex07.generateStringComparatorの検証
  */
 public class Ex07Test {
-    String[] words;
-    private boolean isReverse;
-    private boolean isCaseInsensitive;
-    private boolean unIncludeSpace;
-    private final String[] inputString = { "00", "03", "0 1", "t3", "t0", "T2" };
+	String[] words;
+	private boolean isReverse;
+	private boolean isCaseInsensitive;
+	private boolean unIncludeSpace;
+	private final String[] inputString = { "00", "03", "0 1", "t3", "t0", "T2" };
 
-    @Before
-    public void setWords() {
-        words = new String[inputString.length];
-        for (int i = 0; i < words.length; i++) {
-            words[i] = inputString[i];
-        }
-        isReverse = isCaseInsensitive = unIncludeSpace = false;
-    }
+	@Before
+	public void setWords() {
+		words = new String[inputString.length];
+		for (int i = 0; i < words.length; i++) {
+			words[i] = inputString[i];
+		}
+		isReverse = isCaseInsensitive = unIncludeSpace = false;
+	}
 
-    @Test
-    public void testNatural() {
-        for (int i = 0; i < words.length; i++) {
-            System.out.println(words[i]);
-        }
-        System.out.println(unIncludeSpace);
-        Arrays.sort(words, Ex07.generateStringComparator(isReverse,
-                isCaseInsensitive, unIncludeSpace));
-        assertThat(words,
-                is(new String[] { "0 1", "00", "03", "T2", "t0", "t3" }));
-    }
+	@Test
+	public void testNatural() {
+		for (int i = 0; i < words.length; i++) {
+			System.out.println(words[i]);
+		}
+		System.out.println(unIncludeSpace);
+		Arrays.sort(words, Ex07.generateStringComparator(isReverse, isCaseInsensitive,
+				unIncludeSpace));
+		assertThat(words, is(new String[] { "0 1", "00", "03", "T2", "t0", "t3" }));
+	}
 
-    @Test
-    public void testReverse() {
-        isReverse = true;
-        Arrays.sort(words, Ex07.generateStringComparator(isReverse,
-                isCaseInsensitive, unIncludeSpace));
-        assertThat(words,
-                is(new String[] { "t3", "t0", "T2", "03", "00", "0 1" }));
-    }
+	@Test
+	public void testReverse() {
+		isReverse = true;
+		Arrays.sort(words, Ex07.generateStringComparator(isReverse, isCaseInsensitive,
+				unIncludeSpace));
+		assertThat(words, is(new String[] { "t3", "t0", "T2", "03", "00", "0 1" }));
+	}
 
-    @Test
-    public void testUnIncludeSpace() {
-        unIncludeSpace = true;
-        Arrays.sort(words, Ex07.generateStringComparator(isReverse,
-                isCaseInsensitive, unIncludeSpace));
-        assertThat(words,
-                is(new String[] { "00", "0 1", "03", "T2", "t0", "t3" }));
-    }
+	@Test
+	public void testUnIncludeSpace() {
+		unIncludeSpace = true;
+		Arrays.sort(words, Ex07.generateStringComparator(isReverse, isCaseInsensitive,
+				unIncludeSpace));
+		assertThat(words, is(new String[] { "00", "0 1", "03", "T2", "t0", "t3" }));
+	}
 
-    @Test
-    public void testIgnoreCase() {
-        isCaseInsensitive = true;
-        Arrays.sort(words, Ex07.generateStringComparator(isReverse,
-                isCaseInsensitive, unIncludeSpace));
-        assertThat(words,
-                is(new String[] { "0 1", "00", "03", "t0", "T2", "t3" }));
-    }
+	@Test
+	public void testIgnoreCase() {
+		isCaseInsensitive = true;
+		Arrays.sort(words, Ex07.generateStringComparator(isReverse, isCaseInsensitive,
+				unIncludeSpace));
+		assertThat(words, is(new String[] { "0 1", "00", "03", "t0", "T2", "t3" }));
+	}
 
-    @Test
-    public void testReverseUnIncludeSpace() {
-        isReverse = isCaseInsensitive = true;
-        Arrays.sort(words, Ex07.generateStringComparator(isReverse,
-                isCaseInsensitive, unIncludeSpace));
-        assertThat(words,
-                is(new String[] { "t3", "t0", "T2", "03", "0 1", "00" }));
-    }
+	@Test
+	public void testReverseUnIncludeSpace() {
+		isReverse = unIncludeSpace = true;
+		Arrays.sort(words, Ex07.generateStringComparator(isReverse, isCaseInsensitive,
+				unIncludeSpace));
+		assertThat(words, is(new String[] { "t3", "t0", "T2", "03", "0 1", "00" }));
+	}
 
-    @Test
-    public void testReverseIgnoreCase() {
-        isReverse = isCaseInsensitive = true;
-        Arrays.sort(words, Ex07.generateStringComparator(isReverse,
-                isCaseInsensitive, unIncludeSpace));
-        assertThat(words,
-                is(new String[] { "t3", "T2", "t0", "03", "00", "0 1" }));
-    }
+	@Test
+	public void testReverseIgnoreCase() {
+		isReverse = isCaseInsensitive = true;
+		Arrays.sort(words, Ex07.generateStringComparator(isReverse, isCaseInsensitive,
+				unIncludeSpace));
+		assertThat(words, is(new String[] { "t3", "T2", "t0", "03", "00", "0 1" }));
+	}
 
-    @Test
-    public void testUnIncludeSapceIgnoreCase() {
-        unIncludeSpace = isCaseInsensitive = true;
-        Arrays.sort(words, Ex07.generateStringComparator(isReverse,
-                isCaseInsensitive, unIncludeSpace));
-        assertThat(words,
-                is(new String[] { "00", "0 1", "03", "t0", "T2", "t3" }));
-    }
+	@Test
+	public void testUnIncludeSapceIgnoreCase() {
+		unIncludeSpace = isCaseInsensitive = true;
+		Arrays.sort(words, Ex07.generateStringComparator(isReverse, isCaseInsensitive,
+				unIncludeSpace));
+		assertThat(words, is(new String[] { "00", "0 1", "03", "t0", "T2", "t3" }));
+	}
 
-    @Test
-    public void testReverseIgnoreCaseUnIncludeSapce() {
-        isReverse = isCaseInsensitive = unIncludeSpace = true;
-        Arrays.sort(words, Ex07.generateStringComparator(isReverse,
-                isCaseInsensitive, unIncludeSpace));
-        assertThat(words,
-                is(new String[] { "t3", "T2", "t0", "03", "0 1", "00" }));
-    }
+	@Test
+	public void testReverseIgnoreCaseUnIncludeSapce() {
+		isReverse = isCaseInsensitive = unIncludeSpace = true;
+		Arrays.sort(words, Ex07.generateStringComparator(isReverse, isCaseInsensitive,
+				unIncludeSpace));
+		assertThat(words, is(new String[] { "t3", "T2", "t0", "03", "0 1", "00" }));
+	}
 }
