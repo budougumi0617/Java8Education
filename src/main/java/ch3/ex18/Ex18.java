98ff9f966b4be3423f41845b6af37db87990e69c
budougumi0617 <budougumi0617@gmail.com>
2014-11-22 22:35:22 +0900

    Done ch3.ex18

---
+++ src/main/java/ch3/ex18/Ex18.java
@@ -0,0 +1,38 @@
+/**
+ * @file
+ * @par File Name:
+ * Ex18.java
+ * @author budougumi0617
+ * @date Created on 2014/11/22
+ */
+package main.java.ch3.ex18;
+
+import java.util.function.Function;
+
+/**
+ * @author budougumi0617
+ * @note 71ページの3.8節「例外の取り扱い」
+ *       のuncheckedメソッドを次の内容に従って実装しなさい。
+ *       具体的には、チェックされる例外をスローするラムダ式から
+ *       Function<T,U>を生成するようにしなさい。
+ *       任意の例外をスローする抽象メソッドを持つ関数型インターフェースを見つけるか、
+ *       作成する必要があることに注意しなさい。
+ */
+public class Ex18 {
+	@FunctionalInterface
+	public interface FunctionEx<T, R> {
+		public R apply(T t) throws Exception;
+	}
+
+	public static <T, U> Function<T, U> unchecked(FunctionEx<T, U> func) {
+		return (t) -> {
+			U result = null;
+			try {
+				result = func.apply(t);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+			return result;
+		};
+	}
+}
---
+++ src/test/java/ch3/ex18/Ex18Test.java
@@ -0,0 +1,57 @@
+/**
+ * @file
+ * @par File Name:
+ * Ex18Test.java
+ * @author budougumi0617
+ * @date Created on 2014/11/22
+ */
+package test.java.ch3.ex18;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import java.util.function.Function;
+
+import main.java.ch3.ex18.Ex18;
+
+import org.junit.Test;
+
+/**
+ * @author budougumi0617
+ * @note no comment
+ */
+public class Ex18Test {
+
+	@Test
+	public void testGetException() {
+		Function<Integer, String> func = Ex18.unchecked((i) -> {
+			int test[] = { 1 };
+			int out = test[3];
+			return i.toString();
+		});
+		try {
+			String result = func.apply(100);
+			fail("Cannot execute line" + result);
+		} catch (Exception e) {
+			String actual = e.getCause().getClass().getName();
+			System.out.println("Got Exception : " + actual);
+			assertThat(actual, is(ArrayIndexOutOfBoundsException.class.getName()));
+		}
+
+	}
+
+	@Test
+	public void testNoThrow() {
+		int input = 100;
+		Function<Integer, String> func = Ex18.unchecked((i) -> {
+			return i.toString();
+		});
+		try {
+			assertThat(func.apply(input), is(String.valueOf(input)));
+		} catch (Exception e) {
+			fail("Exception is not expected");
+		}
+
+	}
+}
