7babcf0868cebce14edbd3d90881b40eb38773bb
budougumi0617 <budougumi0617@gmail.com>
2014-12-22 16:58:41 +0900

    Add util method for unit test

--- src/test/java/util/TestOperation.java
+++ src/test/java/util/TestOperation.java
@@ -18,25 +18,51 @@ import java.lang.reflect.Method;
 public class TestOperation {
 
 	public static Object getPrivateField(Object target_object, String field_name)
-			throws Exception {
+			throws ReflectiveOperationException {
 		Class c = target_object.getClass();
+		System.out.println("Class name = " + c);
 		Field fld = c.getDeclaredField(field_name);
 		fld.setAccessible(true);
-
 		return fld.get(target_object);
 	}
 
+	/**
+	 * static privateフィールドの値を取得する関数
+	 * 
+	 * @param targetClass
+	 *            Class name
+	 * @param field_name
+	 *            target class
+	 * 
+	 * @return value of static private field
+	 * @throws ReflectiveOperationException
+	 */
+	public static Object getPrivateField(Class targeClass, String field_name)
+			throws ReflectiveOperationException {
+		Field fld = targeClass.getDeclaredField(field_name);
+		fld.setAccessible(true);
+		return fld.get(null);
+	}
+
 	public static void setPrivateField(Object target_object, String field_name,
-			Object value) throws Exception {
+			Object value) throws ReflectiveOperationException {
 		Class c = target_object.getClass();
 		Field fld = c.getDeclaredField(field_name);
 		fld.setAccessible(true);
 		fld.set(target_object, value);
 	}
 
+	public static void setPrivateField(Class targetClass, String field_name, Object value)
+			throws ReflectiveOperationException {
+
+		Field fld = targetClass.getDeclaredField(field_name);
+		fld.setAccessible(true);
+		fld.set(null, value);
+	}
+
 	@SuppressWarnings("unchecked")
 	public static Object doPrivateMethod(Object targetObject, String methodName,
-			Class[] classArray, Object[] args) throws Exception {
+			Class[] classArray, Object[] args) throws ReflectiveOperationException {
 
 		Class c = targetObject.getClass();
 
