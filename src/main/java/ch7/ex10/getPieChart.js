a2aa2934bc237a971fc7ebbecd1280c1485c536c
budougumi0617 <budougumi0617@gmail.com>
2015-02-24 00:35:06 +0900

    Refactoring getPieChart.js

--- src/main/java/ch7/ex10/getPieChart.js
+++ src/main/java/ch7/ex10/getPieChart.js
@@ -2,26 +2,28 @@ Files = java.nio.file.Files
 Paths = java.nio.file.Paths
 StandardCharsets = java.nio.charset.StandardCharsets
 Arrays = java.util.Arrays
-function csv2Array(filePath) {
-    var csvData = new Array();
-data = new java.lang.String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
-    var lines = data.split('\n');
+
+function csv2Data(filePath) {
+    csvData = new Array()
+        data = new java.lang.String(
+                Files.readAllBytes(Paths.get(filePath)),
+                StandardCharsets.UTF_8)
+    lines = data.split('\n')
     print('lines length : ' + lines.length)
     print('lines : ' + lines)
-    for (var i = 0; i < lines.length;++i) {
-        var cells = lines[i].split(",");
-        if( cells.length != 1 ) {
-            csvData.push(
- new javafx.scene.chart.PieChart.Data(cells[0], cells[1])
-                    );
-        }
+    for (i = 0; i < lines.length;++i) {
+        cells = lines[i].split(",")
+            if( cells.length != 1 ) {
+                csvData.push(
+                        new javafx.scene.chart.PieChart.Data(cells[0], cells[1])
+                        )
+            }
     }
-    return csvData;
+    return csvData
 }
 
-//filePath = 'data.csv'
-filePath = arguments[0]
-datas = csv2Array(filePath)
+filePath = (arguments[0] != null) ? arguments[0] : 'data.csv'
+datas = csv2Data(filePath)
 list = javafx.collections.FXCollections.observableList(datas)
 
 graph = new javafx.scene.chart.PieChart(list)
